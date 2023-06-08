1a6fe3c310659cec178c3ecb388dfd1152a162c9
Nathalie Bartel <ddlyss@gmail.com>
2023-06-08 13:55:08 +0200

    lab react

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import icone1 from './assets/images/icone1'    
+import icone2 from './assets/images/icone2'       
+import icone3 from './assets/images/icone3'      
+import icone4 from './assets/images/icone4'  
+import logo from './assets/images/logo'  
+import menu from './assets/images/menu' 
+       
+
+
 
 function App() {
 
 
   return (
-    <div className="App"></div>
+    <div className="App">
+<nav><img src={logo} alt="logo" /><img src={menu} alt="menu" /></nav>
+<h1>Say hello to ReactJS</h1>
+<h4>Say you will learn </h4>
+    </div>
   );
 }
 
